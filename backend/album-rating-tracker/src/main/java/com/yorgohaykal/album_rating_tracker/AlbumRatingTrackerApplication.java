0343e351cf27be8d5bbf9a1b9fc5f421c7a4974e
Yorgo Haykal <yorgohaykal004@gmail.com>
2026-08-14 15:03:00 +0300

    initializing spring boot

---
+++ backend/album-rating-tracker/src/main/java/com/yorgohaykal/album_rating_tracker/AlbumRatingTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.yorgohaykal.album_rating_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumRatingTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumRatingTrackerApplication.class, args);
+	}
+
+}
